4da0868e71dc09c47cb541400bed3a4383511de8
Erdinc <erdinckolukisa@gmail.com>
2022-09-13 14:50:43 +0200

    Initial Commit

---
+++ MobiquityCodeTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MobiquityCodeTask
+//
+//  Created by Erdinc Kolukisa on 9/13/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
